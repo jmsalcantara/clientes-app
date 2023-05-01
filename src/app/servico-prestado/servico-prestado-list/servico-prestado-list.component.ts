4327ff82e893534dd35e95ff76e08b31a6f0ecd2
João Marcos <joao.alcantara@pdcase.com.br>
2023-05-01 19:56:05 -0300

    Finaliza modulo de serviços prestados

---
+++ src/app/servico-prestado/servico-prestado-list/servico-prestado-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ServicoPrestadoBusca } from './ServicoPrestadoBusca';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
+
+@Component({
+  selector: 'app-servico-prestado-list',
+  templateUrl: './servico-prestado-list.component.html',
+  styleUrls: ['./servico-prestado-list.component.css']
+})
+export class ServicoPrestadoListComponent implements OnInit {
+
+  nome: string;
+  mes: number;
+  meses: number[];
+  lista: ServicoPrestadoBusca[];
+  message: string;
+
+  constructor(
+    private service: ServicoPrestadoService
+    ) {
+      this.meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+   }
+
+  ngOnInit(): void {
+  }
+
+  consultar() {
+    this.service.buscar(this.nome, this.mes)
+      .subscribe(response => {
+        this.lista = response;
+        if (this.lista.length <= 0) {
+          this.message = 'Nenhum registro encontrado.'
+        } else {
+          this.message = null;
+        }
+      });
+  }
+
+}
